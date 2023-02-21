e93764b1cde2d1137ac5a10ca23ea604be9a1ff7
Matheus Borges <matheusborges.engenharia@gmail.com>
2023-02-21 18:28:15 -0300

    first commit

---
+++ js/elements.js
@@ -0,0 +1,15 @@
+const buttonHome = document.querySelector(".button-home")
+const buttonTheUniverse = document.querySelector(".button-universe")
+const buttonExploration = document.querySelector(".button-exploration")
+const buttonKnowMore = document.querySelector(".button-know-more")
+const bodyBackground = document.querySelector("body")
+const mainText = document.querySelector("#app")
+
+export {
+    buttonHome,
+    buttonTheUniverse,
+    buttonExploration,
+    buttonKnowMore,
+    bodyBackground,
+    mainText
+}
